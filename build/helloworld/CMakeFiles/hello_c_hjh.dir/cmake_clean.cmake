fdf25294d67942f7c0c1eb74532ee265d30ea7ca
neuqh <neuhjh@163.com>
2023-05-19 12:21:21 +0800

    11111

---
+++ build/helloworld/CMakeFiles/hello_c_hjh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_c_hjh.dir/src/hello_c_hjh.cpp.o"
+  "/home/dasheng/neu_ws/devel/lib/helloworld/hello_c_hjh.pdb"
+  "/home/dasheng/neu_ws/devel/lib/helloworld/hello_c_hjh"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_c_hjh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
